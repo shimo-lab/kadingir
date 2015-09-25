119a9c331b416742423ae30741cf7d4fbff4e276
oshikiri <t.oshikiri.0137@gmail.com>
2015-09-25 18:53:32 +0900

    Change test settings

--- run_eigenwords.R
+++ run_eigenwords.R
@@ -7,12 +7,12 @@ dim.internal <- 200   # 共通空間の次元
 window.size <- 2      # 前後何個の単語をcontextとするか
 
 ## Making train data
-f <- file("train_all.txt", "r")
+f <- file("data/text8", "r")
 line <- readLines(con = f, -1)
 close(f)
 
 sentence.orig.full <- unlist(strsplit(tolower(line), " "))
-sentence.orig <- sentence.orig.full#[1:100000]
+sentence.orig <- sentence.orig.full[1:1000000]
 vocab.orig <- unique(sentence.orig)
 
 ## Eigenwords
