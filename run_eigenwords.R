e18462397e22fd8de91af1611c784440e01d9c48
oshikiri <t.oshikiri.0137@gmail.com>
2015-09-28 19:05:06 +0900

    Change settings

--- run_eigenwords.R
+++ run_eigenwords.R
@@ -2,7 +2,7 @@
 source("eigenwords.R")
 
 ## Tuning parameters
-min.count <- 10       # 出現回数がmin.count回以下の単語はvocabに入れない
+min.count <- 100      # 出現回数がmin.count回以下の単語はvocabに入れない
 dim.internal <- 200   # 共通空間の次元
 window.size <- 2      # 前後何個の単語をcontextとするか
 
@@ -12,10 +12,10 @@ line <- readLines(con = f, -1)
 close(f)
 
 sentence.orig.full <- unlist(strsplit(tolower(line), " "))
-sentence.orig <- sentence.orig.full[1:5000000]
+sentence.orig <- sentence.orig.full#[1:1000000]
 
 ## Eigenwords
 res.eigenwords <- eigenwords(sentence.orig, min.count, dim.internal, window.size)
 
 ## Check vector representations
-most.similar(res.eigenwords, positive=c("統計"), topn=10)
+most.similar(res.eigenwords, positive=c("man"), topn=10)
