6bae9928adea91710844977e4ffcae162d22bcad
oshikiri <t.oshikiri.0137@gmail.com>
2015-10-15 01:44:47 +0900

    Replace 0L -> T, 1L -> F
    But it didnt work well

--- eigenwords.R
+++ eigenwords.R
@@ -105,7 +105,7 @@ Eigenwords <- function(sentence.orig, min.count = 10,
     indices <- indices[indices[ , 2] > 0, ]
     
     W <- sparseMatrix(i = indices[ , 1], j = indices[ , 2],
-                      x = rep(1L, times = nrow(indices)),
+                      x = rep(T, times = nrow(indices)),
                       dims = c(n.train.words, n.vocab))
 
     cat("Size of W :")
@@ -113,8 +113,8 @@ Eigenwords <- function(sentence.orig, min.count = 10,
     
     ## Construction of C
     offsets <- c(-window.size:-1, 1:window.size)
-    
-    C <- sparseMatrix(i = 1, j = 1, x = 0L, dims = c(n.train.words, 2*window.size*n.vocab))
+    C <- sparseMatrix(i = 1, j = 1, x = F, dims = c(n.train.words, 2*window.size*n.vocab))
+    browser()
     for (i.offset in seq(offsets)) {
         offset <- offsets[i.offset]
         indices.temp <- cbind(seq(sentence) - offset,
@@ -122,7 +122,7 @@ Eigenwords <- function(sentence.orig, min.count = 10,
         indices.temp <- indices.temp[(indices.temp[ , 1] > 0) & (indices.temp[ , 1] <= n.train.words), ]
         indices.temp <- indices.temp[indices.temp[ , 2] > 0, ]
         
-        C.temp <- sparseMatrix(i = indices.temp[, 1], j = indices.temp[, 2], x = rep(1L, times = nrow(indices.temp)),
+        C.temp <- sparseMatrix(i = indices.temp[, 1], j = indices.temp[, 2], x = rep(T, times = nrow(indices.temp)),
                                dims = c(n.train.words, 2*window.size*n.vocab))
         
         C <- C + C.temp
