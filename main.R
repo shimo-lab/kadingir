e4c1a30a0116492354ed02e327eea5b9777e16b3
oshikiri <t.oshikiri.0137@gmail.com>
2015-11-15 22:47:25 +0900

    Change settings of experiments

--- main.R
+++ main.R
@@ -8,11 +8,12 @@ source("eigenwords.R")
 n.vocabulary <- 100000 # 語彙に含める単語数
 dim.internal <- 200   # 共通空間の次元
 window.size <- 4      # 前後何個の単語をcontextとするか
-path.corpus <- "data/enwiki1GB.txt"
+path.corpus <- "data/reuters/reuters_rcv1_text.csv"
+#path.corpus <- "data/text8"
 
 
-                             mode = "tscca", use.block.matrix=FALSE)
 res.eigenwords <- Eigenwords(path.corpus, n.vocabulary, dim.internal, window.size,
+                             mode = "oscca", use.block.matrix=FALSE)
 save(res.eigenwords, file = "res_eigenwords.Rdata")
 
 
