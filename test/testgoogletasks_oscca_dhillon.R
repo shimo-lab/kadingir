416255ba4f47ecea85f814f8ba29805a3134cff6
oshikiri <t.oshikiri.0137@gmail.com>
2015-11-26 11:11:26 +0900

    Fix bugs of testgoogletasks_oscca_dhillon.R

--- test/testgoogletasks_oscca_dhillon.R
+++ test/testgoogletasks_oscca_dhillon.R
@@ -1,15 +1,19 @@
+library(data.table)
+
 source("eigenwords.R")
 
 
-n.use.vocab <- 10000
 
-table <- read.table("data/rcv1.tscca.100k.200.c10", sep = " ")
-table <- table[1:n.use.vocab, ]
-vocab <- as.vector(table[ , 1])
-vectors <- as.matrix(table[ , 2:201])
-class(vectors) <- "numeric"
+n.use.vocab <- 30000
+
+vectors <- fread("data/rcv1.tscca.100k.200.c10.vectors", sep = " ", header = FALSE)
+vectors <- as.matrix(vectors)
+vocab <- readLines("data/rcv1.tscca.100k.200.c10.vocab")
+
+vectors <- vectors[seq(2, n.use.vocab), ]
+vocab <- vocab[seq(2, n.use.vocab)]
 
 MostSimilar(vectors, vocab, positive = c("of"), distance = "cosine")
 MostSimilar(vectors, vocab, positive = c("Japan"), distance = "cosine")
 
-TestGoogleTasks(vectors, vocab, "test/questions-words.txt", n.cores = 12)
+TestGoogleTasks(vectors, vocab, "test/questions-words.txt", n.cores = 24)
