bcfd796de8bd8da4c0166ccbec4562d3a33ebcab
oshikiri <t.oshikiri.0137@gmail.com>
2015-11-26 17:12:31 +0900

    Fix test_wordsim353

--- test/testwordsim353_myeigenwords.R
+++ test/testwordsim353_myeigenwords.R
@@ -1,13 +1,15 @@
 source("eigenwords.R")
 
 
-n.use.vocab <- 100001
-
+## Load data
 load("res_eigenwords.Rdata")
-vocab <- res.eigenwords$vocab.words[1:n.use.vocab]
-vectors <- res.eigenwords$svd$U[1:n.use.vocab, ]
+vocab <- res.eigenwords$vocab.words
+vectors <- res.eigenwords$svd$U
 rownames(vectors) <- vocab
 
+
+
+## Calculate similarities and correlation
 wordsims <- read.csv("data/wordsim353/combined.csv", header = 1)
 
 cosine.similarity <- function (w1, w2) {
@@ -18,13 +20,25 @@ cosine.similarity <- function (w1, w2) {
 }
 
 n.tests <- nrow(wordsims)
-similarity.eigenwords <- rep(0, times = n.tests)
+similarity.eigenwords <- rep(NULL, times = n.tests)
 
 for(i in seq(n.tests)) {
-  w <- wordsims[i, ]
-  similarity.eigenwords[i] <- cosine.similarity(w[[1]], w[[2]])
+  w1 <- as.character(wordsims[i, 1])
+  w2 <- as.character(wordsims[i, 2])
+  
+  if (w1 %in% vocab && w2 %in% vocab) {
+    similarity.eigenwords[i] <- cosine.similarity(w1, w2)
+  }
+  print(w)
+  print(similarity.eigenwords[i])
+  cat("\n\n")
 }
 similarity.human <- wordsims[ , 3]
+
+print(!is.na(similarity.eigenwords))
+
+similarity.human <- similarity.human[!is.na(similarity.eigenwords)]
+similarity.eigenwords <- similarity.eigenwords[!is.na(similarity.eigenwords)]
 
 cor(similarity.human, similarity.eigenwords, method = "spearman")
 plot(similarity.human, similarity.eigenwords)
