918f778403e021780d7fbabbd3c6b595244bd2c2
oshikiri <t.oshikiri.0137@gmail.com>
2015-11-26 18:02:59 +0900

    Fix path of combined.csv

--- eigenwords.R
+++ eigenwords.R
@@ -280,7 +280,7 @@ TestGoogleTasks <- function (U, vocab, path, n.cores = 1) {
 }
 
 
-TestWordsim353 <- function (vectors, path = "data/wordsim353/combined.csv") {
+TestWordsim353 <- function (vectors, path = "test/combined.csv") {
   
   cosine.similarity <- function (w1, w2) {
     v1 <- vectors[w1, ]
