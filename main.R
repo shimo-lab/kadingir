69bd78a516346ffcc03ef550db04d2b6e5eede11
oshikiri <t.oshikiri.0137@gmail.com>
2015-12-02 05:59:00 +0900

    Split mode argument

--- main.R
+++ main.R
@@ -8,11 +8,12 @@ source("eigenwords.R")
 n.vocabulary <- 100000 # 語彙に含める単語数
 dim.internal <- 200   # 共通空間の次元
 window.size <- 10      # 前後何個の単語をcontextとするか
+mode <- "oscca"
 path.corpus <- "data/reuters/reuters_rcv1_text.csv"
 #path.corpus <- "data/text8"
 
 
-res.eigenwords <- Eigenwords(path.corpus, n.vocabulary, dim.internal, window.size, mode = "oscca")
+res.eigenwords <- Eigenwords(path.corpus, n.vocabulary, dim.internal, window.size, , mode = mode)
 save(res.eigenwords, file = "res_eigenwords.Rdata")
 
 
