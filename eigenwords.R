f92bd8a76939a603509caa0d04e6a7813ac40be9
oshikiri <t.oshikiri.0137@gmail.com>
2015-12-14 20:59:44 +0900

    Fix paths

--- eigenwords.R
+++ eigenwords.R
@@ -366,7 +366,7 @@ MostSimilarDocs <- function (document.id, document_vector, titles, topn = 10) {
 }
 
 
-TestGoogleTasks <- function (U, vocab, path = "test/questions-words.txt", n.cores = 1) {
+TestGoogleTasks <- function (U, vocab, path = "./../test/questions-words.txt", n.cores = 1) {
   
   time.start <- Sys.time()
   
@@ -403,7 +403,7 @@ TestGoogleTasks <- function (U, vocab, path = "test/questions-words.txt", n.core
 }
 
 
-TestWordsim353 <- function (vectors, vocab, path = "test/combined.csv") {
+TestWordsim353 <- function (vectors, vocab, path = "./../test/combined.csv") {
   
   cosine.similarity <- function (w1, w2) {
     v1 <- vectors[w1, ]
