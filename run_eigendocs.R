69578cd9fcae3b34e0df5239469d7d190aeb84ef
oshikiri <t.oshikiri.0137@gmail.com>
2015-12-23 13:48:27 +0900

    Change settings

--- run_eigendocs.R
+++ run_eigendocs.R
@@ -5,11 +5,10 @@ source("kadingir.R")
 
 
 ## Tuning parameters
-n.vocabulary <- 100000 # 語彙に含める単語数
+n.vocabulary <- 10000 # 語彙に含める単語数
 dim.internal <- 200   # 共通空間の次元
 window.size <- 4      # 前後何個の単語をcontextとするか
-mode <- "oscca"
-path.corpus <- "data/reuters/reuters_rcv1_text100000.csv"
+path.corpus <- "data/reuters_rcv1_text10000.csv"
 #path.corpus <- "data/train.txt"
 
 
@@ -18,7 +17,7 @@ save(res.eigendocs, file = "res_eigendocs.Rdata")
 
 ## Check vector representations of documents
 document.id <- 999
-infos <- read.csv("data/reuters/reuters_rcv1_infos.csv")
+infos <- read.csv("data/reuters_rcv1_infos.csv")
 MostSimilarDocs(document.id, res.eigendocs$svd$document_vector, titles = infos$title)
 
 
