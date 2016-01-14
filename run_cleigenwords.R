fe4f7ad0efcfc207f0542b5e9d989d8c489c84a1
oshikiri <t.oshikiri.0137@gmail.com>
2016-01-15 00:14:25 +0900

    run_cleigenwords.R で使うコーパスを de-en コーパスから es-en コーパスに変更する

--- run_cleigenwords.R
+++ run_cleigenwords.R
@@ -2,12 +2,12 @@
 source("src/kadingir.R", chdir = TRUE)
 source("src/cleigenwords.R", chdir = TRUE)
 
-paths.corpus <- c("data/europarl/europarl-v7.de-en.de.tokenized",
-                  "data/europarl/europarl-v7.de-en.en.tokenized")
+paths.corpus <- c("data/europarl/europarl-v7.es-en.es.tokenized",
+                  "data/europarl/europarl-v7.es-en.en.tokenized")
 
 r <- CLEigenwords(paths.corpus, sizes.vocabulary = c(10000, 8000),
                   dim.common = 100,
-                  sizes.window = c(2, 2), aliases.languages=c("de", "en"),
+                  sizes.window = c(2, 2), aliases.languages=c("es", "en"),
                   plot = TRUE,
                   weighting_tf = FALSE,
                   weight.vsdoc = c(1.0, 1.0))
@@ -17,17 +17,17 @@ save(r, file = "res_cleigenwords.Rdata")
 p <- r$svd$p_head_domains
 V.de <- r$svd$V[p[1]:p[2], ]
 V.en <- r$svd$V[(p[3]+1):p[4], ]
-vocab.words.de <- paste0("(de)", r$vocab.words[[1]])
+vocab.words.de <- paste0("(es)", r$vocab.words[[1]])
 vocab.words.en <- paste0("(en)", r$vocab.words[[2]])
 
 V <- rbind(V.de, V.en)
 vocab.words <- c(vocab.words.de, vocab.words.en)
 
 MostSimilar(V, vocab.words, positive=c("(en)he"), distance = "cosine", language.search = "en")
-MostSimilar(V, vocab.words, positive=c("(en)he"), distance = "cosine", language.search = "de")
-MostSimilar(V, vocab.words, positive=c("(de)Ich"), distance = "cosine", language.search = "en")
-MostSimilar(V, vocab.words, positive=c("(de)Ich"), distance = "cosine", language.search = "de")
-MostSimilar(V, vocab.words, positive=c("(en)him", "(de)ich"), negative=c("(en)he"), distance = "cosine")
+MostSimilar(V, vocab.words, positive=c("(en)he"), distance = "cosine", language.search = "es")
+MostSimilar(V, vocab.words, positive=c("(es)yo"), distance = "cosine", language.search = "en")
+MostSimilar(V, vocab.words, positive=c("(es)yo"), distance = "cosine", language.search = "es")
+MostSimilar(V, vocab.words, positive=c("(en)him", "(es)yo"), negative=c("(en)he"), distance = "cosine")
 
 
 TestGoogleTasks(r$svd$V[(p[3]+1):p[4], ], r$vocab.words[[2]], n.cores = 12)
