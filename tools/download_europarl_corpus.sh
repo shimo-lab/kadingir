4456f2b562a6906df6f80a59832ff5c148fe7e7d
oshikiri <t.oshikiri.0137@gmail.com>
2016-04-01 17:54:14 +0900

    Add tools/download_europarl_corpus.sh

---
+++ tools/download_europarl_corpus.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# bash download_corpora.sh
+
+cd ./../corpora
+
+# Download Europarl corpora (v7) from http://www.statmt.org/europarl/
+mkdir europarl
+cd europarl
+wget -i ./../../tools/europarl-v7_list_corpus.txt
+cd -
